f3e919961bc2801468771354e3962dcf5241791c
mrkietzer <mrkietzer@gmail.com>
2024-05-16 18:55:30 -0500

    Fixed schemo issue

--- backend/models/transaction.model.js
+++ backend/models/transaction.model.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const Schema = mongoose.Schema;
 const transactionSchema = new mongoose.Schema({
     amount: {
         type: Number,
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken')
 
