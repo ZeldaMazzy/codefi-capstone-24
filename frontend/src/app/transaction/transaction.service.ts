6e75d75fbac2d1789424437a008fae0a869a43c8
mrkietzer <131722826+mrkietzer@users.noreply.github.com>
2024-05-23 19:47:09 -0500

    Routed transaction and account services (#28)

--- frontend/src/app/transaction/transaction.service.ts
+++ frontend/src/app/transaction/transaction.service.ts
@@ -1,47 +1,39 @@
 import { Injectable } from '@angular/core';
 import { Transaction } from './transaction.model';
-import { EMPTY, Observable, findIndex, of } from 'rxjs';
-import { TEST_DATA_TRANSACTION } from '../stub/transaction.stub';
-import { TEST_DATA_ACCOUNTDATA } from '../stub/account.stub';
-import { Account } from '../accounts/accounts.model';
-
+import { Observable, map } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 
-
 export class TransactionService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private router: Router) { }
+
+  private endpoint: string = "http://localhost:2077/api/v1/transactions"
+
   getTransactions(accountID: string): Observable<Transaction[]> {
-    return of (TEST_DATA_TRANSACTION)
+    return this.http.get<any>(this.endpoint + '?account=' + accountID)
+      .pipe(map(r => r.data))
   }
 
-  getTransactionByID(_id: string): Observable<Transaction | null> {
-    const transaction: Transaction | undefined = TEST_DATA_TRANSACTION.find(transaction => transaction._id === _id);
-    return of(transaction || null);
-  }
-
-  addTransaction(addTransaction: Transaction): Observable<Transaction> {
-    TEST_DATA_TRANSACTION.push(addTransaction);
-    return of(addTransaction)
+  getTransactionByID(TransactionID: string): Observable<Transaction | null> {
+    return this.http.get<any | null>(this.endpoint + '/' + TransactionID)
+      .pipe(map(r => r.data))
   }
 
   updateTransaction(transactionToChange: Transaction): Observable<Transaction> {
-    const transactionIndex: number = TEST_DATA_TRANSACTION.findIndex(transaction => {
-      return transaction._id === transactionToChange._id;
-    })
-    TEST_DATA_TRANSACTION[transactionIndex] = transactionToChange;
-    return of(transactionToChange);
+    return this.http.put<any>(this.endpoint + '/' + transactionToChange._id, transactionToChange)
+      .pipe(map(r => r.data))
+  }
+  addTransaction(addTransaction: Transaction): Observable<Transaction> {
+    return this.http.post<any>(this.endpoint, addTransaction)
+      .pipe(map(r => r.data))
   }
 
   deleteTransaction(deleteTransaction: Transaction): Observable<void> {
-    const transactionIndex: number = TEST_DATA_TRANSACTION.findIndex(transaction => {
-      return transaction._id === deleteTransaction._id;
-    })
-    TEST_DATA_TRANSACTION.splice(transactionIndex);
-    return EMPTY;
+    return this.http.delete<void>(this.endpoint + '/' + deleteTransaction._id)
   }
-
 }
